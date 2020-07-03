ad663b3ad859101004abdee8338880d2383972a6
Pavel <46760758+Pashtetickus@users.noreply.github.com>
2020-07-03 20:19:56 +0300

    Delete utils.py

--- utils.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-import numpy 
-
-def save_image(data):
-    img = data.clone().cpu().clamp(0, 255).numpy()
-    img = img.transpose(1, 2, 0).astype("uint8")
-    img = Image.fromarray(img)
-    return img
